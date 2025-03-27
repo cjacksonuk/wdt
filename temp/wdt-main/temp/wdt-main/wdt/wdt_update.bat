94fc70f230f74d9299400a6c563ab4fb1a915a1a
Christian Jackson <cj@icthero.co.uk>
2025-03-27 17:10:30 +0000

    minor

--- temp/wdt-main/temp/wdt-main/wdt/wdt_update.bat
+++ temp/wdt-main/temp/wdt-main/wdt/wdt_update.bat
@@ -12,7 +12,7 @@ echo Root path is: %rootPath%
 :: CONFIG
 set "REPO_USER=cjacksonuk"
 set "REPO_NAME=wdt"
-set "LOCAL_VERSION_FILE=%rootPath%\wdt\wdt_version.txt"
+set "LOCAL_VERSION_FILE=%scriptRoot%\wdt\wdt_version.txt"
 set "TEMP_DIR=%rootPath%temp"
 set "ZIP_URL=https://github.com/%REPO_USER%/%REPO_NAME%/archive/refs/heads/main.zip"
 set "REMOTE_VERSION_URL=https://raw.githubusercontent.com/%REPO_USER%/%REPO_NAME%/main/wdt/wdt_version.txt"
@@ -71,7 +71,7 @@ echo from %TEMP_DIR%\%REPO_NAME%-main to %scriptRoot%
 robocopy "%TEMP_DIR%\%REPO_NAME%-main" %rootPath% /E /NFL /NDL /NJH /NJS /NC
 
 :: Step 7: Cleanup
-rd /s /q "%TEMP_DIR%"
+:::rd /s /q "%TEMP_DIR%"
 echo Update complete! Now running version %REMOTE_VERSION%.
 pause
 exit /b 0
