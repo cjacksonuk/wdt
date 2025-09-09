3f3b4e1fc3424e87ee6b90180e1cdf8075de0e95
cjacksonuk <cj@icthero.co.uk>
2025-09-09 14:09:32 +0100

    Update prerun.ps1

--- wdt/prerun.ps1
+++ wdt/prerun.ps1
@@ -3,6 +3,9 @@ Adding this following code to trust expired certificates on websites
 namely https://onegetcdn.azureedge.net/providers/ which expires every 3months and by chance stopped a enrolment working.
 See also https://patchmypc.com/blog/no-match-was-found-while-installing-the-nuget-packageprovider/
 #>
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 add-type @"
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
