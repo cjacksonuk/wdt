f2b73e0cf6d043d10e6972ff16ebf5687fbadb09
cjacksonuk <cj@icthero.co.uk>
2025-09-09 13:36:58 +0100

    Implement trust for expired certificates in prerun.ps1
    required to assist install powershell packages eg Nuget when ongetcdn.azureedge.net certificate expires.

---
+++ wdt/prerun.ps1
@@ -0,0 +1,18 @@
+<#
+Adding this following code to trust expired certificates on websites
+namely https://onegetcdn.azureedge.net/providers/ which expires every 3months and by chance stopped a enrolment working.
+See also https://patchmypc.com/blog/no-match-was-found-while-installing-the-nuget-packageprovider/
+#>
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
